0c0161943ef408c74e3b073008c876f0e36b892d
Abdullah Daulatzai <abdullahdaulatzai1@gmail.com>
2024-12-18 23:33:22 +1300

    add responsive design style

--- client/components/AboutMe.tsx
+++ client/components/AboutMe.tsx
@@ -2,16 +2,16 @@ export default function AboutMe() {
   return (
     <>
       <div className="py-10 text-center">
-        <div className=" font-audiowide flex flex-col items-start justify-center space-y-2">
-          <div className="text-shadow place-content-around font-sans text-4xl font-bold tracking-wide lg:text-3xl">
+        <div className="font-audiowide flex flex-col items-center justify-center space-y-2">
+          <div className="text-shadow place-content-around font-sans text-4xl font-bold tracking-wide sm:text-3xl md:text-4xl lg:text-3xl">
             <h1 className="font-audiowide inline text-white">Hello,</h1>
-            <h2 className="ml-32 inline font-bold text-white ">
-              FULL STACK DEVELOPER
+            <h2 className="mt-4 font-bold text-white sm:mt-2">
+              I'm <span className="text-blue-500">Abdullah!</span>
             </h2>
           </div>
-          <div className="mt-2">
-            <h2 className=" text-2xl font-bold tracking-widest  text-white lg:text-4xl">
-              I'm <span className="text-blue-500">Abdullah!</span>
+          <div className="mt-2 sm:mt-2 md:mt-4 lg:mt-4">
+            <h2 className="text-2xl font-bold tracking-widest text-white sm:text-3xl md:text-4xl lg:text-4xl">
+              FULL STACK DEVELOPER
             </h2>
           </div>
         </div>
