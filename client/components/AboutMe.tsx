c5c963575f454fde1e926511cebdd4ac99dc74ab
Abdullah Daulatzai <abdullahdaulatzai1@gmail.com>
2024-12-18 23:25:32 +1300

    center About Me component content

--- client/components/AboutMe.tsx
+++ client/components/AboutMe.tsx
@@ -5,13 +5,13 @@ export default function AboutMe() {
         <div className=" font-audiowide flex flex-col items-start justify-center space-y-2">
           <div className="text-shadow place-content-around font-sans text-4xl font-bold tracking-wide lg:text-3xl">
             <h1 className="font-audiowide inline text-white">Hello,</h1>
-            <h2 className="ml-32 inline font-bold text-pink-500">
+            <h2 className="ml-32 inline font-bold text-white ">
               FULL STACK DEVELOPER
             </h2>
           </div>
           <div className="mt-2">
-            <h2 className="text-2xl font-bold tracking-widest text-blue-300 lg:text-4xl">
-              I'm Abdullah!
+            <h2 className=" text-2xl font-bold tracking-widest  text-white lg:text-4xl">
+              I'm <span className="text-blue-500">Abdullah!</span>
             </h2>
           </div>
         </div>
