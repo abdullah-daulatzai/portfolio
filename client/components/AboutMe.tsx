907e6dfc33f794b538c9d389bbae2dbfb946333e
Abdullah Daulatzai <abdullahdaulatzai1@gmail.com>
2024-12-19 23:21:48 +1300

    add additional responsive design utility classes to About Me component

--- client/components/AboutMe.tsx
+++ client/components/AboutMe.tsx
@@ -2,7 +2,7 @@ export default function AboutMe() {
   return (
     <>
       <div className="py-10 text-center">
-        <div className="font-audiowide flex flex-col items-center justify-center space-y-2">
+        <div className="font-audiowide flex flex-row items-center justify-center space-y-2">
           <div className="text-shadow place-content-around font-sans text-4xl font-bold tracking-wide sm:text-3xl md:text-4xl lg:text-3xl">
             <h1 className="font-audiowide inline text-white">Hello,</h1>
             <h2 className="mt-4 font-bold text-white sm:mt-2">
@@ -10,7 +10,7 @@ export default function AboutMe() {
             </h2>
           </div>
           <div className="mt-2 sm:mt-2 md:mt-4 lg:mt-4">
-            <h2 className="text-2xl font-bold tracking-widest text-white sm:text-3xl md:text-4xl lg:text-4xl">
+            <h2 className="ml-2 text-xl tracking-wider text-white sm:ml-8 sm:text-3xl md:ml-16 md:text-4xl lg:text-2xl">
               FULL STACK DEVELOPER
             </h2>
           </div>
