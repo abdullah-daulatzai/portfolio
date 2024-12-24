3821a438998e10b0cec8bc0e9b33f3ff43657a38
Abdullah Daulatzai <abdullahdaulatzai1@gmail.com>
2024-12-24 19:43:36 +1300

    add card style in Project components

--- client/components/Project.tsx
+++ client/components/Project.tsx
@@ -1,8 +1,16 @@
 export default function Project() {
   return (
     <>
-      <div>
-        <div>Abdullah</div>
+      <div className="justify-even flex h-screen flex-row gap-12 p-12">
+        <div className=" flex h-[70%] w-[30%] flex-col rounded-md border border-gray-800 bg-[#141414] p-20">
+          Abdullah
+        </div>
+        <div className=" flex h-[70%] w-[30%] flex-col rounded-md border border-gray-800 bg-[#141414] p-20">
+          Abdullah
+        </div>
+        <div className=" flex h-[70%] w-[30%] flex-col rounded-md border border-gray-800 bg-[#141414] p-20">
+          Abdullah
+        </div>
       </div>
     </>
   )
