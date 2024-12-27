59677653f19b33361bb4fdca8e91d132d8cb44b0
Abdullah Daulatzai <abdullahdaulatzai1@gmail.com>
2024-12-27 23:33:32 +1300

    add input fields

--- client/components/Contact.tsx
+++ client/components/Contact.tsx
@@ -1,7 +1,16 @@
 export default function Contact() {
   return (
     <>
-      <div>Abdullah Contact</div>
+      <div className=" flex flex-col">
+        <div className="flex w-14 flex-col">
+          <label htmlFor=""> Name</label>
+          <input type="text" />
+        </div>
+        <div className="flex w-14 flex-col">
+          <label htmlFor=""> Email</label>
+          <input type="text" />
+        </div>
+      </div>
     </>
   )
 }
