679701710bd9afac8749cb48cb35456b9f65db68
Abdullah Daulatzai <abdullahdaulatzai1@gmail.com>
2024-12-30 23:41:18 +1300

    Fix JSX syntax error by moving comments outside of class attributes in AboutMe component.

--- client/components/AboutMe.tsx
+++ client/components/AboutMe.tsx
@@ -3,7 +3,7 @@ export default function AboutMe() {
     <>
       <div className="py-10" id="about-me">
         <div className="sm:text-md flex flex-col items-center justify-center space-y-2 font-audiowide md:flex-row">
-          <div className="text-shadow font-sans  font-bold tracking-wide sm:text-3xl md:text-4xl lg:text-3xl">
+          <div className="text-shadow font-sans font-bold tracking-wide sm:text-3xl md:text-4xl lg:text-3xl">
             <h1 className="inline font-audiowide text-4xl text-white">
               Kia ora,
             </h1>
@@ -11,26 +11,28 @@ export default function AboutMe() {
               I'm <span className="text-blue-500 ">Abdullah!</span>
             </h2>
           </div>
-          <div className="mt-2 sm:mt-2 md:mt-4  lg:mt-4">
+          <div className="mt-2 sm:mt-2 md:mt-4 lg:mt-4">
             <h2 className="font-montserrat text-2xl tracking-wider text-white sm:text-3xl md:text-4xl lg:ml-16 lg:text-2xl">
               FULL STACK DEVELOPER
             </h2>
           </div>
         </div>
+
         <div className="mt-10 flex flex-col gap-y-10 md:flex-row md:gap-x-10">
-          <div className="flex-none self-center md:ml-[3%] lg:ml-[15%]">
+          <div className="mb-16 h-[350px] w-[270px] flex-none self-center md:ml-[3%] lg:ml-[15%]">
             <img
               src="public/Myimg.png"
               alt="Photo"
-              className=" h-auto w-full max-w-xs border-4 border-double border-green-700 bg-gray-400 object-cover hover:bg-blue-900 md:w-60 lg:pt-20"
+              className="h-full w-full border-4 border-double border-green-700 bg-gray-400 object-cover hover:bg-blue-900 md:w-96 lg:w-[500px] lg:pt-20"
             />
           </div>
-          <div className=" max-w-md px-8 sm:px-2 md:px-0 lg:mt-32">
+
+          <div className="max-w-md px-8 sm:px-2 md:px-0 lg:mt-32">
             <h3 className="font-audiowide text-3xl font-semibold text-blue-300">
               About Me
             </h3>
-            <p className="lg:text-md sm:text-md mt-4 max-w-xl  text-justify  text-gray-300">
-              I’m Abdullah a Full-Stack Developer based in Auckland with a
+            <p className="sm:text-md lg:text-md mt-4 max-w-xl text-justify text-gray-200">
+              I’m Abdullah, a Full-Stack Developer based in Auckland with a
               degree in Computer Science and Technology. My journey into
               software development has been driven by a love for solving
               problems, creating smart solutions, and constantly learning
@@ -43,9 +45,9 @@ export default function AboutMe() {
               event, chatting with like-minded people. Thanks for stopping by my
               site!
             </p>
-            <div className="mt-[10%] ">
+            <div className="mt-[10%]">
               <a
-                className="  hover:brightness-15  rounded-md bg-gradient-to-r from-blue-500 to-blue-800 pb-2 pl-5 pr-5 pt-2 text-white transition-all duration-300 hover:scale-110 hover:text-yellow-500"
+                className="hover:brightness-15 rounded-md bg-gradient-to-r from-blue-500 to-blue-800 pb-2 pl-5 pr-5 pt-2 text-white transition-all duration-300 hover:scale-110 hover:text-yellow-500"
                 href="https://drive.google.com/file/d/16LQcXlhu0A5csPnSUvJ5OPx4cKiOLgPR/view?usp=sharing"
               >
                 My CV
