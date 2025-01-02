b7d3a2491260e8bdc5ab9d2bea0930efae0dbabf
Abdullah Daulatzai <abdullahdaulatzai1@gmail.com>
2025-01-02 23:42:16 +1300

    Added hover effect with scale-105 and purple-900 background

--- client/components/Project.tsx
+++ client/components/Project.tsx
@@ -2,7 +2,7 @@ export default function Project() {
   return (
     <>
       <div className="justify-even flex flex-wrap justify-center gap-12 p-12">
-        <div className="flex w-full flex-col rounded-md border border-gray-800 bg-[#141414] p-6 sm:w-full md:w-1/3 lg:w-1/4">
+        <div className="flex w-full flex-col rounded-md border border-gray-800 bg-[#141414] p-6 hover:scale-105 hover:bg-purple-900 sm:w-full md:w-1/3 lg:w-1/4">
           <img
             src="https://via.placeholder.com/300"
             alt="Project Image"
@@ -18,8 +18,7 @@ export default function Project() {
           </p>
         </div>
 
-        {/* Card 2 */}
-        <div className="flex w-full flex-col rounded-md border border-gray-800 bg-[#141414] p-6 sm:w-full md:w-1/3 lg:w-1/4">
+        <div className="flex w-full flex-col rounded-md border border-gray-800 bg-[#141414] p-6 hover:scale-105 hover:bg-purple-900 sm:w-full md:w-1/3 lg:w-1/4">
           <img
             src="https://via.placeholder.com/300"
             alt="Project Image"
@@ -35,8 +34,7 @@ export default function Project() {
           </p>
         </div>
 
-        {/* Card 3 */}
-        <div className="flex w-full flex-col rounded-md border border-gray-800 bg-[#141414] p-6 sm:w-full md:w-1/3 lg:w-1/4">
+        <div className="flex w-full flex-col rounded-md border border-gray-800 bg-[#141414] p-6 hover:scale-105 hover:bg-purple-900 sm:w-full md:w-1/3 lg:w-1/4">
           <img
             src="https://via.placeholder.com/300"
             alt="Project Image"
