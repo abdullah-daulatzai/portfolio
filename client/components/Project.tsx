c638b925ade045f97e6464f5cad96b3b97da2466
Abdullah Daulatzai <abdullahdaulatzai1@gmail.com>
2025-01-03 00:12:27 +1300

    add weekwise image in Project component and change the container size

--- client/components/Project.tsx
+++ client/components/Project.tsx
@@ -2,11 +2,11 @@ export default function Project() {
   return (
     <>
       <div className="justify-even flex flex-wrap justify-center gap-12 p-12">
-        <div className="flex w-full flex-col rounded-md border border-gray-800 bg-[#141414] p-6 hover:scale-105 hover:bg-purple-900 sm:w-full md:w-1/3 lg:w-1/4">
+        <div className="flex w-full flex-col rounded-md border border-gray-800 bg-[#141414] p-6 hover:scale-105 hover:bg-purple-900 sm:w-full md:w-1/3 lg:w-[28%]">
           <img
-            src="https://via.placeholder.com/300"
-            alt="Project Image"
-            className="h-48 w-full rounded-md object-cover"
+            src="public/weekwise.png"
+            alt="WeekWise"
+            className="h-50  w-[600px]  rounded-md"
           />
           <h3 className="mt-4 text-xl font-semibold text-white">
             Project Title 1
