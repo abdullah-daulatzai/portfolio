114598350fb1c67fc3704cc9656602ae96171fa6
Abdullah Daulatzai <abdullahdaulatzai1@gmail.com>
2025-01-06 23:07:44 +1300

    add photo under second project container

--- client/components/Project.tsx
+++ client/components/Project.tsx
@@ -71,13 +71,11 @@ export default function Project() {
 
         <div className="flex w-full flex-col rounded-md border border-gray-800 bg-[#141414] p-6 hover:scale-105 hover:bg-purple-900 sm:w-full md:w-1/3 lg:w-1/4">
           <img
-            src="https://via.placeholder.com/300"
-            alt="Project Image"
+            src="public/Stalgic.png"
+            alt="Stalgic"
             className="h-48 w-full rounded-md object-cover"
           />
-          <h3 className="mt-4 text-xl font-semibold text-white">
-            Project Title 2
-          </h3>
+          <h3 className="mt-4 text-xl font-semibold text-white">Stalgic</h3>
           <p className="mt-2 text-gray-400">
             This is a description of the project. You can include details about
             what the project is about, technologies used, and any other relevant
