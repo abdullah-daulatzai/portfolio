7ce5189257286550b40795fed2b58e97655b8852
Abdullah Daulatzai <abdullahdaulatzai1@gmail.com>
2025-01-08 18:45:42 +1300

    create Stalgic component and add a router path to Staltic

---
+++ client/components/ProjectDetails/Stalgic.tsx
@@ -0,0 +1,16 @@
+import Nav from '../Nav'
+
+export default function Stalgic() {
+  return (
+    <>
+      <header className="flex w-full justify-center bg-[#000040] shadow-lg shadow-[#000040]/50">
+        <Nav />
+      </header>
+      <div className="flex flex-col">
+        <div>
+          <h1> Stalgic </h1>
+        </div>
+      </div>
+    </>
+  )
+}
--- client/routes.tsx
+++ client/routes.tsx
@@ -1,3 +1,9 @@
 import { createRoutesFromElements, Route } from 'react-router-dom'
 import App from './components/App.tsx'
-export default createRoutesFromElements(<Route index element={<App />} />)
+import Stalgic from './components/ProjectDetails/Stalgic.tsx'
+export default createRoutesFromElements(
+  <>
+    <Route index element={<App />} />
+    <Route path="/stalgic" element={<Stalgic />} />
+  </>,
+)
